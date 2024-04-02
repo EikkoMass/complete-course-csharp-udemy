f5e61cc1bf018c8d1e6726701a96a014f79fba4b
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-02 14:02:03 -0300

    Conversão Implicita e Casting

---
+++ 2_recapitulacao/ImplicitAndCasting/ImplicitAndCasting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImplicitAndCasting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a;
+            float b;
+            int c;
+
+            a = 5.1;
+            b = (float) a;
+            c = (int) a;
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            int d = 5;
+            int e = 2;
+
+            double resultado = (double) d / e;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
