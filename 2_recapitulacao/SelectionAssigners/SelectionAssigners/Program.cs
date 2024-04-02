d903fd033bfabbf5905486e6c815a16cceca7847
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-02 11:38:09 -0300

    Operadores de atribuição

---
+++ 2_recapitulacao/SelectionAssigners/SelectionAssigners/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SelectionAssigners
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            int a2 = 10;
+            a2++;
+            Console.WriteLine(a2);
+
+            a2--;
+            Console.WriteLine(a2);
+
+            Console.WriteLine(a2++);
+            Console.WriteLine(a2);
+
+            Console.WriteLine(++a2);
+            Console.WriteLine(a2);
+
+        }
+    }
+}
