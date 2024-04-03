9efc09a4a3371cc13ca17893c5f400893a32cc6b
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-03 08:42:09 -0300

    For Loops em C#

---
+++ 2_recapitulacao/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write($"Valor #{i}: ");
+                soma += int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Soma: " + soma);
+        }
+    }
+}
