67b0ff426583e14cc47ff14a0da93d315a25e8c6
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-09 16:09:53 -0300

    sobrecarga de classe (no construtor)

---
+++ 4_construtores_this_sobrecarga_encapsulamento/Sobrecarga/Sobrecarga/Program.cs
@@ -0,0 +1,38 @@
+﻿using Sobrecarga;
+using System;
+using System.Globalization;
+
+namespace Constutores
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionados: ");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removidos: ");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados " + p);
+        }
+    }
+}
