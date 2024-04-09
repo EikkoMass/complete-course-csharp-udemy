2d43fb573938da9c922216a7fc200ba67ca67e68
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-09 09:57:30 -0300

    Membros estáticos (versão 2)

---
+++ 3_classes-atributos-metodos-membros_estaticos/MembrosEstaticosV2/MembrosEstaticosV2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticosV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculadora calc = new Calculadora();
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double .Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Circunferência: " + Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Circunferência: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
