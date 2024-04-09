8b70c64a8f7a14725df99468983a524dfeef52c5
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-09 15:15:33 -0300

    exercicio sobre membros estáticos

---
+++ 3_classes-atributos-metodos-membros_estaticos/CotacaoDolar/CotacaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotacaoDolar
+{
+    internal class ConversorDeMoeda
+    {
+        public static double CotacaoDolar;
+
+        public static double ValorASerPago(double dolares)
+        {
+            double valueToBuy = dolares * CotacaoDolar;
+
+            return valueToBuy + (valueToBuy * 0.06);
+        }
+
+
+    }
+}
---
+++ 3_classes-atributos-metodos-membros_estaticos/CotacaoDolar/CotacaoDolar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            ConversorDeMoeda.CotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Valor a ser pago em reais = " + ConversorDeMoeda.ValorASerPago(dolares).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
