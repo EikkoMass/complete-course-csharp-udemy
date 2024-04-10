3c9056e3cc2571e543790b199c7102d6966e55d1
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-10 16:37:08 -0300

    Exercicio Fixação

---
+++ 4_construtores_this_sobrecarga_encapsulamento/ExercicioFixacao/ExercicioFixacao/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    internal class Conta
+    {
+
+        public int Numero { get; private set; }
+        public string Titular { get; private set;}
+        public double Saldo { get; private set; }
+
+        public Conta(string titular, int numero, double deposito) : this(titular, numero)
+        {
+            AplicarDeposito(deposito);
+        }
+
+        public Conta(string titular, int numero)
+        {
+            Titular = titular;
+            Numero = numero;
+        }
+
+        public void AplicarDeposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void AplicarSaque(double saque)
+        {
+            Saldo -= (saque + 5);
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ 4_construtores_this_sobrecarga_encapsulamento/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char dep = char.Parse(Console.ReadLine());
+            Conta conta;
+
+
+            if ('s' == dep || 'S' == dep)
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                conta = new Conta(titular, numero, double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            }else
+            {
+                conta = new Conta(titular, numero);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            conta.AplicarDeposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.Write("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: ");
+            conta.AplicarSaque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.Write("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
