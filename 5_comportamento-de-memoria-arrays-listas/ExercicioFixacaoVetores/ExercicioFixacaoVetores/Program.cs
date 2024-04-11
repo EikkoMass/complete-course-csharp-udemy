a62c044d4f5a82c1783992f3a15a7c84996a5a5a
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-11 10:09:58 -0300

    Exercicio Fixação Vetores

---
+++ 5_comportamento-de-memoria-arrays-listas/ExercicioFixacaoVetores/ExercicioFixacaoVetores/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace ExercicioFixacaoVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantRooms = 10;
+
+            Console.Write("How many rooms will be rented? ");
+            int quantRents = int.Parse(Console.ReadLine());
+
+            Room[] rooms = new Room[quantRooms];
+
+            for (int i = 0; i < quantRents; i++) {
+
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i + 1}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int number = int.Parse(Console.ReadLine());
+
+                rooms[number] = new Room(name, email, number);
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for(int i = 0; i < quantRooms; i++)
+            {
+                if (rooms[i] != null)
+                {
+                    Console.WriteLine(rooms[i]);
+                }
+            }
+        }
+    }
+}
---
+++ 5_comportamento-de-memoria-arrays-listas/ExercicioFixacaoVetores/ExercicioFixacaoVetores/Room.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacaoVetores
+{
+    internal class Room
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Number {  get; set; }
+
+        public Room() { }
+
+        public Room(string name, string email, int number) {
+
+            Name = name;
+            Email = email;
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return $"{Number}: {Name}, {Email}";
+        }
+    }
+}
