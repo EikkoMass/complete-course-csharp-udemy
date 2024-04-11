cdf013d83e08edc42c4b959ef9c9c76f8d9805ca
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-11 09:39:02 -0300

    Vetores

---
+++ 5_comportamento-de-memoria-arrays-listas/Vetores/Vetores/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            exercicio1();
+            exercicio2();
+        }
+
+        private static void exercicio1()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i];
+            }
+
+            Console.WriteLine($"AVERAGE HEIGHT = {(soma / n).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        private static void exercicio2() {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Product
+                {
+                    Name = name,
+                    Price = price
+                };
+            }
+
+            double sum = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine($"AVERAGE PRICE = {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
