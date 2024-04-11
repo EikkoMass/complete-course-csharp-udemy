9961e134a7df0992753197d73a3ca73093efdb48
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-11 10:41:25 -0300

    Modificadores de parâmetros ref e out

---
+++ 5_comportamento-de-memoria-arrays-listas/ModificadorRefOut/ModificadorRefOut/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ModificadorRefOut
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+
+            int b = 10;
+            int tripleB;
+            Calculator.Triple(b, out tripleB);
+            Console.WriteLine(tripleB);
+        }
+    }
+}
