ed00693dcac2e84d80131271f9d05350cd956ec3
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-15 10:48:40 -0300

    Inferência de tipos (var)

---
+++ 6_topicos-essenciais-em-csharp/InferenciaTiposVar/InferenciaTiposVar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenciaTiposVar
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            var w = z;
+            // int  = z;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y); 
+            Console.WriteLine(z);
+        }
+    }
+}
