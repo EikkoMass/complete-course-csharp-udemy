ba609bbe414eed10f17b6ddb41e64b359318caf1
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-15 11:57:16 -0300

    Sintaxe Alternativa: Expressão Condicional para string

---
+++ 6_topicos-essenciais-em-csharp/CondicionalTernaria/CondicionalTernaria/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionalTernaria
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto;
+
+            /*
+                if(preco < 20.0)
+                {
+                    desconto = preco * 0.1;
+                } else
+                {
+                    desconto = preco * 0.05;
+                }
+            */
+
+            desconto = preco * (preco < 20.0 ? 0.1 : 0.05); 
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
--- 6_topicos-essenciais-em-csharp/CondicionalTernaria/CondicionalTernaria/obj/Debug/net6.0/CondicionalTernaria.AssemblyInfo.cs
+++ 6_topicos-essenciais-em-csharp/CondicionalTernaria/CondicionalTernaria/obj/Debug/net6.0/CondicionalTernaria.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SwitchCase")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CondicionalTernaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2fd82912aa06fa63b76f18bed25138962af7b90")]
-[assembly: System.Reflection.AssemblyProductAttribute("SwitchCase")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SwitchCase")]
+[assembly: System.Reflection.AssemblyProductAttribute("CondicionalTernaria")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CondicionalTernaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
