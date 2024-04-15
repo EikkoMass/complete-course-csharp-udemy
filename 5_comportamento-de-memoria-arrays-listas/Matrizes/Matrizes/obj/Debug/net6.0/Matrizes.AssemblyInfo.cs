d2fd82912aa06fa63b76f18bed25138962af7b90
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-15 11:18:50 -0300

    removendo diretorios de configuração

--- 5_comportamento-de-memoria-arrays-listas/Matrizes/Matrizes/obj/Debug/net6.0/Matrizes.AssemblyInfo.cs
+++ 5_comportamento-de-memoria-arrays-listas/Matrizes/Matrizes/obj/Debug/net6.0/Matrizes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Matrizes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9310bf879b9647e158264df0a409994368aab977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f79901cc8621804bef8a28d2b15f12b75d008a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Matrizes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Matrizes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
