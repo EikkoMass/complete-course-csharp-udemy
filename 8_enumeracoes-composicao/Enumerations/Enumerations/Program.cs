d9a381da9f63e8b9b23034f6508ec30213c13d59
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-04-30 15:57:56 -0300

    Enumerações

---
+++ 8_enumeracoes-composicao/Enumerations/Enumerations/Program.cs
@@ -0,0 +1,25 @@
+﻿using Enumerations.Entities;
+using Enumerations.Entities.Enums;
+
+namespace Enumerations;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Order order = new Order
+        {
+            Id = 1000,
+            Moment = DateTime.Now,
+            Status = Entities.Enums.OrderStatus.PendingPayment
+        };
+
+        Console.WriteLine(order);
+
+        string txt = OrderStatus.PendingPayment.ToString();
+
+        OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+        Console.WriteLine(os);
+        Console.WriteLine(txt);
+    }
+}
