d72ee90a212adba19d63b6a91e5423398969398b
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-05-08 09:57:34 -0300

    Classes e Metodos Selados

---
+++ 9_heranca-polimorfismo/ClassesMetodosSelados/ClassesMetodosSelados/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMetodosSelados.Entities
+{
+    // não vai funcionar o extends se SavingsAccount possuir 'sealed'
+    internal class SavingsAccountPlus : SavingsAccount
+    {
+
+
+        // Não vai funcionar pois SavingsAccount aplica 'sealed' no método Withdraw
+        /*
+        public sealed override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 3;
+        }
+        */
+    }
+}
