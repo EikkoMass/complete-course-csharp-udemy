42c1aee39053d468cf14c3ad1efd8017324a14df
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-05-07 23:08:27 -0300

    Sobreposição, Virtual, Override e Base

--- 9_heranca-polimorfismo/SobreposicaoVirtualOverride/SobreposicaoVirtualOverride/obj/Debug/net6.0/SobreposicaoVirtualOverride.AssemblyInfo.cs
+++ 9_heranca-polimorfismo/SobreposicaoVirtualOverride/SobreposicaoVirtualOverride/obj/Debug/net6.0/SobreposicaoVirtualOverride.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Upcasting-Downcasting")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SobreposicaoVirtualOverride")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9418a89d8e0997179089b8c59257187f30bb76")]
-[assembly: System.Reflection.AssemblyProductAttribute("Upcasting-Downcasting")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Upcasting-Downcasting")]
+[assembly: System.Reflection.AssemblyProductAttribute("SobreposicaoVirtualOverride")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SobreposicaoVirtualOverride")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
