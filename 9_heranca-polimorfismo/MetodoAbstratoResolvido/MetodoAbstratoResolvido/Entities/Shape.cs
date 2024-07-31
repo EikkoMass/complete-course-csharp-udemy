c826053b6433ac4a6a5e15ff37c14648e18670a3
Eric Henrique Massaneiro <eric.massaneiro@fulltechpro.com.br>
2024-07-31 15:21:44 -0300

    Exercicio Resolvido (Metodos Abstratos)

---
+++ 9_heranca-polimorfismo/MetodoAbstratoResolvido/MetodoAbstratoResolvido/Entities/Shape.cs
@@ -0,0 +1,16 @@
+﻿using MetodoAbstratoResolvido.Entities.Enums;
+
+namespace MetodoAbstratoResolvido.Entities
+{
+    abstract class Shape
+    {
+        protected Color Color { get; set; }
+
+        /* Se adicionar constructor base tira obrigatoriedade de criar constructor (mesmo em classe abstrata) */
+         // public Shape() { }
+
+        public Shape(Color color) { Color = color; }
+
+        public abstract double Area();
+    }
+}
---
+++ 9_heranca-polimorfismo/MetodoAbstratoResolvido/MetodoAbstratoResolvido/Program.cs
@@ -0,0 +1,52 @@
+﻿using MetodoAbstratoResolvido.Entities;
+using MetodoAbstratoResolvido.Entities.Enums;
+using System.Globalization;
+
+namespace MetodoAbstratoResolvido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            Console.Write("Enter the number of shapes: ");
+            int numOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                Console.WriteLine($"Shape #{i+1} data:");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char shapeType = char.Parse(Console.ReadLine());
+
+                Console.Write("Color (Black/Blue/Red): ");
+                Color color = (Color) Enum.Parse(typeof(Color), Console.ReadLine());
+
+                if(shapeType == 'r' || shapeType == 'R')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    shapes.Add(new Rectangle(width, height, color));
+                } else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    shapes.Add(new Circle(radius, color));
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("SHAPE AREAS: ");
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine(shape.Area().ToString("F2"));
+            }
+        }
+    }
+}
