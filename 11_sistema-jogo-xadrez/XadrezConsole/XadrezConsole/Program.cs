de768515ae46893734a2c11903b029560ee78e55
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-08-08 19:18:37 -0300

    Criando a classe posição (Criando a primeira classe do projeto)

--- 11_sistema-jogo-xadrez/XadrezConsole/XadrezConsole/Program.cs
+++ 11_sistema-jogo-xadrez/XadrezConsole/XadrezConsole/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace XadrezConsole
+﻿using tabuleiro;
+
+namespace XadrezConsole
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao posicao = new Posicao(3, 4);
+
+            Console.WriteLine($"Posição: {posicao}");
         }
     }
 }
