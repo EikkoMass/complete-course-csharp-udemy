459c268247c2fca979993f508323ba923c3aebcf
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-07 00:12:41 -0300

    File, FileInfo e IOException

---
+++ 11_trabalhando-com-arquivos/FileFileInfoIOException/FileFileInfoIOException/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FileFileInfoIOException;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = @"../../../temp/file1.txt";
+        string targetPath = @"../../../temp/file2.txt";
+
+        try
+        {
+            FileInfo fileInfo = new FileInfo(sourcePath);
+            fileInfo.CopyTo(targetPath);
+            
+            string[] lines = File.ReadAllLines(sourcePath);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An Error occured!");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
