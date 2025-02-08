bbae50000df3b38992dd7b7b8b9c92819e352952
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-08 14:02:49 -0300

    FileStream e StreamReader

---
+++ 11_trabalhando-com-arquivos/FileStreamStreamReader/FileStreamStreamReader/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FileStreamStreamReader;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"../../../temp/file1.txt";
+        StreamReader streamReader = null;
+
+        try
+        {
+            streamReader = File.OpenText(path);
+            
+            while (!streamReader.EndOfStream)
+            {
+                Console.WriteLine(streamReader.ReadLine());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An error occured!");
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            streamReader?.Close();
+        }
+    }
+}
