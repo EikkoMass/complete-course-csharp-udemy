cb03c9d09de978b6fc35a04676a3b8af401ea92c
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-09 12:47:37 -0300

    Bloco Using

---
+++ 11_trabalhando-com-arquivos/BlocoUsing/BlocoUsing/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BlocoUsing;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"../../../temp/file1.txt";
+
+        try
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error ocurred");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
