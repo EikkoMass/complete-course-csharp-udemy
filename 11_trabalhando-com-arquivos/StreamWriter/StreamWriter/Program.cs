611d59901babf430ae334d223bd0f59be4540765
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-10 14:11:20 -0300

    StreamWriter

---
+++ 11_trabalhando-com-arquivos/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StreamWriter;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = @"../../../temp/file1.txt";
+        string targetPath = @"../../../temp/file2.txt";
+
+        try
+        {
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            using (System.IO.StreamWriter sw = File.AppendText(targetPath))
+            {
+                foreach (string line in lines) sw.WriteLine(line.ToUpper());
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occured!");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
