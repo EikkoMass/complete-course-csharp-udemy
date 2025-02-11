66c2ba32b748c87d32cb819f09843d0c76cf077a
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-11 20:13:32 -0300

    Directory, DirectoryInfo

---
+++ 11_trabalhando-com-arquivos/DirectoryDirectoryInfo/DirectoryDirectoryInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DirectoryDirectoryInfo;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"../../../temp/myfolder";
+
+        try
+        {
+            var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+            Console.WriteLine($"Folders:");
+            foreach (var folder in folders) Console.WriteLine(folder);
+            
+            var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            Console.WriteLine($"Files:");
+            foreach (var file in files) Console.WriteLine(file);
+            
+            Directory.CreateDirectory($"{path}/newFolder");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
