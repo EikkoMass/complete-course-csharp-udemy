0b50897c49ab44917fa5d8ac457f47090abd41b3
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-12 20:30:21 -0300

    Path

---
+++ 11_trabalhando-com-arquivos/Path/Path/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Path;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"../../../temp/myfolder/file1.txt";
+        
+        Console.WriteLine($"DirectorySeparatorChar: {System.IO.Path.DirectorySeparatorChar}");
+        Console.WriteLine($"PathSeparator: {System.IO.Path.PathSeparator}");
+        Console.WriteLine($"GetDirectoryName: {System.IO.Path.GetDirectoryName(path)}");
+        Console.WriteLine($"GetFileName: {System.IO.Path.GetFileName(path)}");
+        Console.WriteLine($"GetExtension: {System.IO.Path.GetExtension(path)}");
+        Console.WriteLine($"GetFileNameWithoutExtension: {System.IO.Path.GetFileNameWithoutExtension(path)}");
+        Console.WriteLine($"GetFullPath: {System.IO.Path.GetFullPath(path)}");
+        Console.WriteLine($"GetTempPath: {System.IO.Path.GetTempPath()}");
+    }
+}
