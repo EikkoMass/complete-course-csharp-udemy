0337b957ee5d654f41145dca8674fa70cb67ec52
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-15 15:57:01 -0300

    Solução sem interface (pt2)

--- 12_interfaces/SolucaoSemInterface/SolucaoSemInterface/Program.cs
+++ 12_interfaces/SolucaoSemInterface/SolucaoSemInterface/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using SolucaoSemInterface.Entities;
+using SolucaoSemInterface.Services;
 
 namespace SolucaoSemInterface;
 
@@ -7,14 +8,25 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter rental data");
+        Console.Write("Enter rental data: ");
         string model = (string) Console.ReadLine();
-        Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+        
+        Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
         DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
-        Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+        Console.Write("Return (dd/MM/yyyy hh:mm): ");
         DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+        Console.Write("Enter price per hour: ");
+        double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Enter price per day: ");
+        double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
         
         CarRental rental = new CarRental(start, finish, new Vehicle(model));
-        
+
+        RentalService rentalService = new RentalService(hour, day);
+        rentalService.ProcessInvoice(rental);
+
+        Console.WriteLine("Invoice: ");
+        Console.WriteLine(rental.Invoice);
     }
 }
