4ebe5fe530325e6e712900c8ef39192cdf38c36a
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-21 20:36:57 -0300

    Generics PARTE 1

---
+++ 13_generics-set-dictionary/Generics/Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using Generics.Services;
+
+namespace Generics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        PrintService ps = new PrintService();
+        Console.Write("How many values? ");
+        int quantity = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < quantity; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            ps.AddValue(x);
+        }
+
+        ps.Print();
+        Console.WriteLine($"First: {ps.First()}");
+    }
+}
