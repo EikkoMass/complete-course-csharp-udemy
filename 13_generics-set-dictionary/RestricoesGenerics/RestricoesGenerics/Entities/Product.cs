0ea1d4f8ce0354a6eb907f10cb553d0bb1d168e5
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-27 16:14:25 -0300

    Restrições de generics

---
+++ 13_generics-set-dictionary/RestricoesGenerics/RestricoesGenerics/Entities/Product.cs
@@ -0,0 +1,28 @@
+using System.Globalization;
+
+namespace RestricoesGenerics.Entities;
+
+public class Product : IComparable
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+
+    public override string ToString()
+    {
+        return $"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if(!(obj is Product)) throw new ArgumentException("Comparing Error: argument is not a Product");
+        Product other = (Product) obj;
+        return Price.CompareTo(other.Price);
+    }
+}
---
+++ 13_generics-set-dictionary/RestricoesGenerics/RestricoesGenerics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using RestricoesGenerics.Entities;
+using RestricoesGenerics.Services;
+
+namespace RestricoesGenerics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> list = new List<Product>();
+        
+        Console.Write("Enter N: ");
+        int quantity = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < quantity; i++)
+        {
+            string[] vect = Console.ReadLine().Split(",");
+            string name = vect[0];
+            double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+            
+            list.Add(new Product(name, price));
+        }
+        
+        CalculationService calculationService = new CalculationService();
+
+        Console.WriteLine($"Max: ");
+        Console.WriteLine(calculationService.Max(list));
+        ;
+    }
+}
