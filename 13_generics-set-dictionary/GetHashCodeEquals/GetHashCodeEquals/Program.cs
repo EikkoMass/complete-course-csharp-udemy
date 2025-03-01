8e3783e1603c8ab39469353b2b6266880fa8d743
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-03-01 14:39:21 -0300

    GetHashCode e Equals

---
+++ 13_generics-set-dictionary/GetHashCodeEquals/GetHashCodeEquals/Program.cs
@@ -0,0 +1,18 @@
+﻿using GetHashCodeEquals.Entities;
+
+namespace GetHashCodeEquals;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
+        Client b = new Client() { Name = "Alex", Email = "alex@gmail.com" };
+
+        Console.WriteLine(a.Equals(b)); // comparando por E-mail (override)
+        Console.WriteLine(a == b); // comparando referencia de memoria
+        Console.WriteLine(a.GetHashCode());
+        Console.WriteLine(b.GetHashCode());
+        Console.WriteLine(a.GetHashCode() == b.GetHashCode()); // Hashcode
+    }
+}
