6199f9d44305b8fd1ff3e65e7bf734c0e5648e99
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-03-11 14:23:15 -0300

    Exercicio proposto sobre conjuntos

---
+++ 13_generics-set-dictionary/ExercicioPropostoConjuntos/ExercicioPropostoConjuntos/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ExercicioPropostoConjuntos;
+
+public class ExercicioPropostoConjuntos
+{
+    static void Main(string[] args)
+    {
+        SortedSet<int> students = new SortedSet<int>();
+        
+        Console.Write("How many students for course A? ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n1; i++)
+        {
+            students.Add(int.Parse(Console.ReadLine()));
+        }
+        
+        Console.Write("How many students for course B? ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n2; i++)
+        {
+            students.Add(int.Parse(Console.ReadLine()));
+        }
+        
+        Console.Write("How many students for course C? ");
+        int n3 = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n3; i++)
+        {
+            students.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.Write($"Total Students: {students.Count}");
+    }
+}
