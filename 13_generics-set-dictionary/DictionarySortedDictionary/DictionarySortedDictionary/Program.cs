8a660b51dda78b153f748b0e834b3f4d2f8359c3
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-03-12 21:19:15 -0300

    Dictionary e SortedDictionary

---
+++ 13_generics-set-dictionary/DictionarySortedDictionary/DictionarySortedDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DictionarySortedDictionary;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+        cookies["user"] = "Maria";
+        cookies["email"] = "maria@gmail.com";
+        cookies["phone"] = "99712234";
+        cookies["phone"] = "83737388";
+
+        Console.WriteLine(cookies["email"]);
+        Console.WriteLine(cookies["phone"]);
+        
+        cookies.Remove("email");
+
+        Console.WriteLine(cookies.ContainsKey("email") ? cookies["email"] : "There is no 'email' key");
+        Console.WriteLine($"Size: {cookies.Count}");
+
+        Console.WriteLine($"All cookies:");
+        foreach (KeyValuePair<string, string> cookie in cookies)
+        {
+            Console.WriteLine($"{cookie.Key}: {cookie.Value}");
+        }
+    }
+}
