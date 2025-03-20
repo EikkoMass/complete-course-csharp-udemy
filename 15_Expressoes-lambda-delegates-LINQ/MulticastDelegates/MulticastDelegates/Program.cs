d2bb50f47ca5accc6a0521c641aa6bcc1777453b
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-03-20 16:33:18 -0300

    Multicast delegates

---
+++ 15_Expressoes-lambda-delegates-LINQ/MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using MulticastDelegates.Services;
+
+namespace MulticastDelegates;
+
+delegate void BinaryNumericOperation(double n1, double n2);
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 10;
+        double b = 12;
+        
+        BinaryNumericOperation op = CalculationService.ShowSum;
+        op += CalculationService.ShowMax; // Delegates podem armazenar multiplas funcoes, rodando todas quando forem chamadas
+        
+        op(a, b);
+        op.Invoke(a, b);
+    }
+}
