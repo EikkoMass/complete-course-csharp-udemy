28e7d204229fbe9593ef7f91501846e16c5cfee2
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-03-19 21:04:04 -0300

    Introducao a delegates

---
+++ 15_Expressoes-lambda-delegates-LINQ/IntroducaoDelegates/IntroducaoDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using IntroducaoDelegates.Services;
+
+namespace IntroducaoDelegates;
+
+delegate double BinaryNumericOperation(double n1, double n2);
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 10;
+        double b = 12;
+
+        BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+        Console.WriteLine(op(a, b));
+
+        op = CalculationService.Max;
+        Console.WriteLine(op(a, b));
+        Console.WriteLine(op.Invoke(a, b)); // Mesmo resultado do print acima
+
+        // op = CalculationService.Square; // Invalido! Assinatura da funcao nao bate com a do delegate
+    }
+}
