88ca3e3d1ad0ee7e1527239e621b3060812f015d
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-03-21 21:22:04 -0300

    Predicate (exemplo com RemoveAll)

---
+++ 15_Expressoes-lambda-delegates-LINQ/PredicateComRemoveAll/PredicateComRemoveAll/Program.cs
@@ -0,0 +1,28 @@
+﻿using PredicateComRemoveAll.Entitites;
+
+namespace PredicateComRemoveAll;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> list = new();
+        
+        list.Add(new Product("TV", 900.00));
+        list.Add(new Product("Mouse",  50.00));
+        list.Add(new Product("Tablet", 350.50));
+        list.Add(new Product("HD Case", 80.90));
+        
+        list.RemoveAll(ProductTest);
+
+        foreach (Product p in list)
+        {
+            Console.WriteLine(p);
+        }
+    }
+
+    public static bool ProductTest(Product p)
+    {
+        return p.Price >= 100;
+    }
+}
