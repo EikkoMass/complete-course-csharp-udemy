6d7d8ae06147ed433806d8e61b89000b0b0b32cd
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-03-24 19:45:41 -0300

    Introdução ao LINQ

---
+++ 15_Expressoes-lambda-delegates-LINQ/IntroducaoLINQ/IntroducaoLINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IntroducaoLINQ;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 2, 3, 4, 5 };
+        
+        IEnumerable<int> result = numbers
+            .Where(x => x % 2 == 0)
+            .Select(x => x * 10);
+
+        foreach (var x in result)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
